fb3ada7308d812584f66cab1386c88aa0900a44d
igor-timchenko <74782054+igor-timchenko@users.noreply.github.com>
2025-01-29 20:08:58 +0700

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,66 +1,62 @@
 import tkinter as tk
 import random
 
-class GuessNumberGame:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Угадай число")
+class GuessTheNumber:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Угадай число")
         
-        self.difficulty_label = tk.Label(root, text="Выберите уровень сложности:")
+        self.difficulty_label = tk.Label(master, text="Выберите уровень сложности:")
         self.difficulty_label.pack()
-
-        self.difficulty_var = tk.StringVar(value='easy')
-        self.easy_radio = tk.Radiobutton(root, text='Легкий (1-10)', variable=self.difficulty_var, value='easy')
-        self.medium_radio = tk.Radiobutton(root, text='Средний (1-50)', variable=self.difficulty_var, value='medium')
-        self.hard_radio = tk.Radiobutton(root, text='Сложный (1-100)', variable=self.difficulty_var, value='hard')
-
-        self.easy_radio.pack()
-        self.medium_radio.pack()
-        self.hard_radio.pack()
-        self.start_button = tk.Button(root, text='Начать игру', command=self.start_game)
-        self.start_button.pack()
-
-        self.guess_label = tk.Label(root, text='Угадайте число:')
-        self.guess_label.pack()
         
-        self.guess_entry = tk.Entry(root)
-        self.guess_entry.pack()
-
-        self.guess_button = tk.Button(root, text='Угадать', command=self.check_guess)
-        self.guess_button.pack()
-
-        self.message_label = tk.Label(root, text='')
+        self.difficulty_var = tk.StringVar(value='10')
+        
+        self.easy_button = tk.Radiobutton(master, text='Легкий (1-10)', variable=self.difficulty_var, value='10')
+        self.medium_button = tk.Radiobutton(master, text='Средний (1-50)', variable=self.difficulty_var, value='50')
+        self.hard_button = tk.Radiobutton(master, text='Сложный (1-100)', variable=self.difficulty_var, value='100')
+        
+        self.easy_button.pack()
+        self.medium_button.pack()
+        self.hard_button.pack()
+        
+        self.start_button = tk.Button(master, text='Начать игру', command=self.start_game)
+        self.start_button.pack()
+        self.message_label = tk.Label(master, text="")
         self.message_label.pack()
-
-        self.secret_number = None
-        self.attempts = 0
+        
+        self.guess_entry = tk.Entry(master)
+        self.guess_entry.pack()
+        
+        self.check_button = tk.Button(master, text='Проверить', command=self.check_guess)
+        self.check_button.pack()
+        
+        self.reset_button = tk.Button(master, text='Сыграть еще раз', command=self.reset_game)
+        self.reset_button.pack()
+        
+        self.reset_game()
 
     def start_game(self):
-        difficulty = self.difficulty_var.get()
-        
-        if difficulty == 'easy':
-            self.secret_number = random.randint(1, 10)
-        elif difficulty == 'medium':
-            self.secret_number = random.randint(1, 50)
-        else:
-            self.secret_number = random.randint(1, 100)
-
+        max_number = int(self.difficulty_var.get())
+        self.random_number = random.randint(1, max_number)
         self.attempts = 0
-        self.message_label.config(text='')
-
+        self.message_label.config(text=f"Я загадал число от 1 до {max_number}. Попробуйте угадать!")
+    
     def check_guess(self):
         guess = int(self.guess_entry.get())
         self.attempts += 1
         
-        if guess == self.secret_number:
-            self.message_label.config(text=f'Поздравляем! Вы угадали число {self.secret_number} за {self.attempts} попыток.')
-            self.guess_entry.delete(0, tk.END)
-        elif guess < self.secret_number:
-            self.message_label.config(text='Слишком низкое число!')
+        if guess < self.random_number:
+            self.message_label.config(text='Слишком мало! Попробуйте снова.')
+        elif guess > self.random_number:
+            self.message_label.config(text='Слишком много! Попробуйте снова.')
         else:
-            self.message_label.config(text='Слишком высокое число!')
+            self.message_label.config(text=f'Поздравляем! Вы угадали число {self.random_number} за {self.attempts} попыток.')
+
+    def reset_game(self):
+        self.message_label.config(text="")
+        self.guess_entry.delete(0, tk.END)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    game = GuessNumberGame(root)
+    game = GuessTheNumber(root)
     root.mainloop()
