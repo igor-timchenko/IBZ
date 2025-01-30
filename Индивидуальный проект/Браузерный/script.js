7059a93fa1f41ea5f19c7d182b6d4256c1ee15a2
igor-timchenko <74782054+igor-timchenko@users.noreply.github.com>
2025-01-30 14:59:23 +0700

    Update script.js

--- Индивидуальный проект/Браузерный/script.js
+++ Индивидуальный проект/Браузерный/script.js
@@ -1,21 +1,21 @@
 let randomNumber;
-   let attempts = 0;
+let attempts = 0;
 
-   document.getElementById('difficulty').addEventListener('change', function() {
-       const max = parseInt(this.value);
-       randomNumber = Math.floor(Math.random() * max) + 1;
-       attempts = 0;
-       document.getElementById('result').textContent = '';
-   });
+document.getElementById('difficulty').addEventListener('change', function() {
+   const max = parseInt(this.value);
+   randomNumber = Math.floor(Math.random() * max) + 1;
+   attempts = 0;
+   document.getElementById('result').textContent = '';
+});
 
-   document.getElementById('submit').addEventListener('click', function() {
-       const guess = parseInt(document.getElementById('guess').value);
-       attempts++;
-       if (guess === randomNumber) {
-           document.getElementById('result').textContent = Поздравляем! Вы угадали число ${randomNumber} за ${attempts} попыток.;
-       } else if (guess < randomNumber) {
-           document.getElementById('result').textContent = 'Слишком маленькое число!';
-       } else {
-           document.getElementById('result').textContent = 'Слишком большое число!';
-       }
-   });
+document.getElementById('submit').addEventListener('click', function() {
+   const guess = parseInt(document.getElementById('guess').value);
+   attempts++;
+   if (guess === randomNumber) {
+      document.getElementById('result').textContent = Поздравляем! Вы угадали число ${randomNumber} за ${attempts} попыток.;
+   } else if (guess < randomNumber) {
+      document.getElementById('result').textContent = 'Слишком маленькое число!';
+   } else {
+      document.getElementById('result').textContent = 'Слишком большое число!';
+   }
+});
