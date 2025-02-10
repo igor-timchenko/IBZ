2f30f6bb28a1b7c3bcdd0d2e37445e5216a529d5
igor-timchenko <74782054+igor-timchenko@users.noreply.github.com>
2025-02-10 11:38:08 +0700

    Update settings.py

--- party_kod/backend/party_kod/settings.py
+++ party_kod/backend/party_kod/settings.py
@@ -1,1 +1,19 @@
+# party_kod/settings.py
 
+INSTALLED_APPS = [
+    ...
+    'channels',
+    'chat',
+]
+
+ASGI_APPLICATION = 'party_kod.asgi.application'
+
+# Настройки Redis (если используете Redis)
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
