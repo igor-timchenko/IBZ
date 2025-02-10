904b8b7a62beff16ae35948b907c8835f73b3138
igor-timchenko <74782054+igor-timchenko@users.noreply.github.com>
2025-02-10 11:43:11 +0700

    Update App.js

--- party_kod/frontend/src/App.js
+++ party_kod/frontend/src/App.js
@@ -1,1 +1,29 @@
+// src/App.js
 
+import React, { useState } from 'react';
+import Chat from './Chat';
+
+const App = () => {
+    const [chatName, setChatName] = useState('');
+    const [isChatStarted, setIsChatStarted] = useState(false);
+
+    const handleStartChat = () => {
+        setIsChatStarted(true);
+    };
+
+    return (
+        <div>
+            {!isChatStarted ? (
+                <div>
+                    <h1>Введите название чата</h1>
+                    <input type="text" onChange={(e) => setChatName(e.target.value)} />
+                    <button onClick={handleStartChat}>Начать чат</button>
+                </div>
+            ) : (
+                <Chat chatName={chatName} />
+            )}
+        </div>
+    );
+};
+
+export default App;
