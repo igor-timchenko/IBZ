5676c9e5dabd25f1cc291489f52acc9bbc7ca005
igor-timchenko <74782054+igor-timchenko@users.noreply.github.com>
2025-02-10 11:39:36 +0700

    Update consumers.py

--- party_kod/backend/chat/consumers.py
+++ party_kod/backend/chat/consumers.py
@@ -1,1 +1,42 @@
+# chat/consumers.py
 
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.chat_name = self.scope['url_route']['kwargs']['chat_name']
+        self.room_group_name = f'chat_{self.chat_name}'
+
+        # Присоединяемся к группе чата
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Отключаемся от группы чата
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']
+
+        # Отправляем сообщение в группу чата
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username,
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        # Отправляем сообщение на WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+        }))
